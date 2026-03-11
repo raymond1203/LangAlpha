ec37e31a48aa9039f2ace94ed134be76b397317d
Alan Chen <zc2610@nyu.edu>
2026-03-10 20:04:15 -0400

    build(web): add TypeScript toolchain configuration

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
   { ignores: ['dist/**', 'public/mockServiceWorker.js'] },
@@ -29,6 +31,41 @@ export default [
     rules: {
       'react/prop-types': 'off',
       'no-unused-vars': ['warn', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_|^React$',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+      },
+    },
+    settings: { react: { version: 'detect' } },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react/prop-types': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_|^React$',
         caughtErrorsIgnorePattern: '^_',
---
+++ web/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL?: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
+  readonly VITE_AUTH_USER_ID?: string;
+  readonly VITE_CDN_BASE?: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
